bb3fbea5c3a201b081fd56489c658ce214973103
Julien <julien.bardin81@gmail.com>
2020-11-22 16:17:26 +0100

    change api adress

--- src/js/utils/api.js
+++ src/js/utils/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-export const servURL = 'http://localhost:3000/'
+export const servURL = 'http://localhost:4000/'
 //config axios for work with cookies
 axios.defaults.withCredentials = true;
 //init an instance with url of api
 const instance = axios.create({
-    baseURL: 'http://localhost:3000/api/'
+    baseURL: 'http://localhost:4000/api/'
 })
 
 

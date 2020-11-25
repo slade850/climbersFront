a480de4a8211c7f12d78a96c1f881f04b1ba7ccd
Julien <julien.bardin81@gmail.com>
2020-11-25 13:03:39 +0100

    add formPop post and comment

--- src/js/pages/dashboard/component/home.js
+++ src/js/pages/dashboard/component/home.js
@@ -1,16 +1,43 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { servURL } from '../../../utils/api';
+import React, { useState, useRef }from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import api, { servURL } from '../../../utils/api';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faThumbsUp, faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faThumbsUp, faHeart, faPlusCircle, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { getPosts } from '../../../store/postStore';
 
 const DashboardHome = () => {
 
+    const dispatch = useDispatch();
+    const formRef = useRef();
     const posts = useSelector(state => state.post.posts);
+    const [title, setTitle] = useState('');
+    const [text, setText] = useState('');
+    const [comment, setComment] = useState('');
+    const [postTargetId, setPostTargetId] = useState('');
+    const [openPop, setOpenPop] = useState({pop: false, type: ''});
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(formRef.current);
+        const body = {
+            comment,
+            post_id: postTargetId
+        }
+        api.post(`${openPop.type == 'post' ? 'post/create' : 'comment/create'}`, openPop.type == 'post' ? data : body, openPop.type == 'post' ? {headers: { 'content-type': 'multipart/form-data' }} : {headers: { 'content-type': 'application/json' }})
+        .then(res => {
+            setTitle('');
+            setText('');
+            setComment('');
+            setPostTargetId('');
+            setOpenPop({pop: false, type: ''})
+            dispatch(getPosts());
+        })
+        .catch(err => console.log(err))
+    }
 
     return (
         <div className="centralContainer">
-            <h1>Bienvenue sur votre page fastblock</h1>
+            <h1 className="centerText">Bienvenue sur votre page fastblock</h1>
             {
                 posts && posts.length > 0 ?
                 posts.map(post => { 
@@ -40,6 +67,16 @@ const DashboardHome = () => {
                                     {post.text}
                             </div>
                         </div>
+                        <div className="likeThisPost">
+                            {
+                                post.likes && post.likes.length > 0 && post.likes.map(like => {
+                                return (<div key={`${like.pseudo}${post.id.split('-')[0]}`} className="likeCard">
+                                            <div className="likeIco">
+                                                { like.type == 'like' ? <FontAwesomeIcon className="likeIco" icon={faThumbsUp} /> : <FontAwesomeIcon className="loveIco" icon={faHeart} />}
+                                            </div>
+                                        </div>)})
+                            }
+                        </div>
                         <div className="commentThisPost">
                             {
                                 post.comments && post.comments.length > 0 && post.comments.map(comment => {
@@ -54,21 +91,53 @@ const DashboardHome = () => {
                                         </div>)})
                             }
                         </div>
-                        <div className="likeThisPost">
-                            {
-                                post.likes && post.likes.length > 0 && post.likes.map(like => {
-                                return (<div key={`${like.pseudo}${post.id.split('-')[0]}`} className="likeCard">
-                                            <div className="likeIco">
-                                                { like.type == 'like' ? <FontAwesomeIcon icon={faThumbsUp} size="2x" color="orange"/> : <FontAwesomeIcon icon={faHeart} size="2x" color="orange"/>}
-                                            </div>
-                                        </div>)})
-                            }
+                        <div className="openPopForm" onClick={() => {
+                            setPostTargetId(post.id);
+                            setOpenPop({pop: true, type: 'comment'})  
+                            }}>
+                            <FontAwesomeIcon className="popIco" icon={faPlusCircle} />
                         </div>
                     </div>)
                 })
                 :
                 <h2 className="noPosts">Aucun Post n'a été publié</h2>
             }
+            {
+            openPop.pop && <>
+            {openPop.type == 'post' ?
+            <form className="fromCardPop" onSubmit={handleSubmit} ref={formRef} >
+                <FontAwesomeIcon className="closePop" icon={faTimes} onClick={() => setOpenPop({pop: false, type: ''})}/>
+                <h1 className="formHeader">Ajoute ton post</h1>
+                <div className="formElement">
+                    <label>Media</label>
+                    <input type="file" name='medias' multiple />
+                </div>
+                <div className="formElement">
+                    <label>Title</label>
+                    <input type="text" onChange={(ev)=> setTitle(ev.target.value)} name='title' value={title} required />
+                </div>
+                <div className="formElement">
+                    <label>Text</label>
+                    <input type="text" onChange={(ev)=> setText(ev.target.value)} minLength="8" name='text' value={text} required />
+                </div>
+                <button type="submit">Envoyer</button><br />
+            </form>
+            :
+            <form className="fromCardPop" onSubmit={handleSubmit} >
+                <FontAwesomeIcon className="closePop" icon={faTimes} onClick={() => setOpenPop({pop: false, type: ''})}/>
+                <h1 className="formHeader">Ajoute ton commentaire</h1>
+                <div className="formElement">
+                    <label>Commentaire</label>
+                    <input type="text" onChange={(ev)=> setComment(ev.target.value)} name='comment' value={comment} required />
+                </div>
+                    <input type="hidden" minLength="8" name='post_id' value={postTargetId} />
+                <button type="submit">Envoyer</button><br />
+            </form>}
+            </>
+            }
+            <div className="openPopForm" onClick={() => setOpenPop({pop: true, type: 'post'}) }>
+                <FontAwesomeIcon className="popIco" icon={faPlusCircle} />
+            </div>
         </div>
             )
 }
